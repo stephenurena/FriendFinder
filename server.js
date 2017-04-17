dfd7b84bda6168f8915a350ea5954d82194a5e20
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-04-17 06:18:23 -0700

    modified server.js file, created server using express

---
+++ server.js
@@ -0,0 +1,25 @@
+//Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Express configuration
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use('/js', express.static('js'))
+
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
