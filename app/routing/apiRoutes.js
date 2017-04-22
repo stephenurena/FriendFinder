e058602e1d80bbbdfae4132be9c64b9a64c8cb83
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-04-21 19:24:46 -0700

    push

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -32,14 +32,8 @@ module.exports = function(app) {
   app.post("/api/tables", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+      friendsData.push(req.body);
       res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
   });
 
   // ---------------------------------------------------------------------------
